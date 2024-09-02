0dca213cfaf9e2b0aaa1ef0f5fe432077e3b37e9
Carol Vasconcelos <carolinavpinheiro@gmail.com>
2024-09-02 20:34:12 +0800

    criando estrutura básica do formulário lidando com os inputs através dos hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from './components/header';
+import RegistrationForm from './components/registrationForm/form';
 
 function App() {
 
   return (
     <div>
       <Header />
+      <RegistrationForm />
     </div>
   )
 }
